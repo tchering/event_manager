27517993c3ec54e94b6268d34d22c064c0191142
tchering <sonam.sherpa.lama@gmail.com>
2024-08-16 20:16:58 +0200

    generate individual thank you email in new file

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,6 +1,7 @@
 require "csv"
 require "google/apis/civicinfo_v2"
 require "pry-byebug"
+require "erb"
 
 file = "../event_attendees.csv"
 
@@ -19,21 +20,34 @@ def legislator_by_zipcode(zipcode)
       roles: ["legislatorUpperBody", "legislatorLowerBody"],
     )
     legislators = legislators.officials
+
     legislator_names = legislators.map do |legislator|
       legislator.name
     end
-    legislator_names.join(",")
-    rescue
-       'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    legislator_names.join(",") #using map method returns array of names there we are using join method to convert the array in string of names.
+  rescue
+    "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
+  end
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("../output") unless Dir.exist?("../output")
+  file_name = "../output/thanks_#{id}.html"
+  File.open(file_name, "w") do |file|
+    file.write(form_letter)
   end
 end
 
 file_content = CSV.open(file, headers: true, header_converters: :symbol)
 file_content.each do |row|
+  id = row[0]
   zipcode = clean_zipcode(row[:zipcode])
   name = row[:first_name]
   legislators = legislator_by_zipcode(zipcode)
-  puts "#{name} #{zipcode} #{legislators}"
+  template = File.read("../template.erb")
+  erb_template = ERB.new(template)
+  form_letter = erb_template.result(binding)
+  save_thank_you_letter(id, form_letter)
 end
 
 # legislator_by_zipcode(4444)
--- lib/test.rb
+++ lib/test.rb
@@ -1,45 +1,7 @@
-require "csv"
-require "google/apis/civicinfo_v2"
-require "pry-byebug"
-require "pp"
+require 'erb'
 
-file = "../event_attendees.csv"
-
-civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-# puts civic_info.methods
-# p civic_info.method(:representative_info_by_address).parameters
-civic_info.key = File.read("../secret.key").strip
-
-def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, "0")[0..4]
-end
-
-def find_legislators(file, civic_info)
-  file_content = CSV.open(file, headers: true, header_converters: :symbol)
-  file_content.each do |row|
-    # binding.pry
-    city = row[:city]
-    state = row[:state]
-    name = row[:first_name]
-    zipcode = clean_zipcode(row[:zipcode])
-    begin
-      legislators = civic_info.representative_info_by_address(
-        address: zipcode,
-        levels: "country",
-        roles: ["legislatorUpperBody", "legislatorLowerBody"],
-      )
-      # puts legislators.methods
-      legislators = legislators.officials
-      # pp legislators
-    rescue
-      puts "You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials"
-      legislators = []
-    end
-    legislators.each do |legislator|
-      puts "#{name} #{city} #{state} #{zipcode} #{legislator.name} #{legislator.party}"
-        # p " #{legislator.instance_variables}"
-    end
-  end
-end
-
-find_legislators(file, civic_info)
+@name = "Sonam"
+template = File.read('../form_letter.erb')
+renderer = ERB.new(template)
+result = renderer.result(binding)
+puts result
