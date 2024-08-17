e8c95f7693f2b33cfe8e58625782e8e9be54f1da
tchering <sonam.sherpa.lama@gmail.com>
2024-08-17 12:48:54 +0200

    Peak registratio hours and registration days

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,11 +2,27 @@ require "csv"
 require "google/apis/civicinfo_v2"
 require "pry-byebug"
 require "erb"
+require "date"
 
 file = "../event_attendees.csv"
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, "0")[0..4]
+end
+
+def clean_number(phone_number)
+  phone_number = phone_number.to_s.gsub(/\D/, "")
+  if phone_number.length < 10 || phone_number.length > 11
+    "0000000000"
+  elsif phone_number.length == 10
+    return phone_number
+  elsif phone_number.length == 11 && phone_number[0] == "1"
+    phone_number.slice(1..-1)
+  elsif phone_number.length == 11 && phone_number[0] != "1"
+    "0000000000"
+  else
+    "0000000000"
+  end
 end
 
 def legislator_by_zipcode(zipcode)
@@ -38,16 +54,36 @@ def save_thank_you_letter(id, form_letter)
   end
 end
 
+registration_hours = Hash.new(0)
+registration_days = Hash.new(0)
+
 file_content = CSV.open(file, headers: true, header_converters: :symbol)
 file_content.each do |row|
-  id = row[0]
+  phone_number = clean_number(row[:homephone])
+  id = row[0] #we need this to generate file_name in thank you letter.
   zipcode = clean_zipcode(row[:zipcode])
   name = row[:first_name]
+
   legislators = legislator_by_zipcode(zipcode)
   template = File.read("../template.erb")
   erb_template = ERB.new(template)
   form_letter = erb_template.result(binding)
-  save_thank_you_letter(id, form_letter)
-end
+  # save_thank_you_letter(id, form_letter)
+  #!extracting hours from the date in csv file
+  registration_date = DateTime.strptime(row[:regdate], "%m/%d/%y %H:%M")
+  registration_hour = registration_date.hour
+  registration_hours[registration_hour] += 1
 
-# legislator_by_zipcode(4444)
+  #!extracting days from the date in csv file
+  registration_day = registration_date.day
+  registration_days[registration_day] += 1
+end
+#Determining peak registration hours
+max_hour, max_count = registration_hours.max_by { |hour, count| count }
+puts "Peak registration hour:"
+puts "Hour #{max_hour}: #{max_count} registration "
+
+#Determining the day most people registered.
+day, count = registration_days.max_by { |day, count| count }
+puts "Peak registration day"
+puts "#{day}: #{count} people"
--- lib/test.rb
+++ lib/test.rb
@@ -1,7 +1,7 @@
-require 'erb'
+require 'date'
 
-@name = "Sonam"
-template = File.read('../form_letter.erb')
-renderer = ERB.new(template)
-result = renderer.result(binding)
-puts result
+date_string = "2023-10-05 14:35:00"
+
+strp_date = DateTime.strptime(date_string, '%Y-%m-%d %H:%M:%S')
+formatted_date = strp_date.strftime('%Y-%m-%d %H:%M')
+puts formatted_date
